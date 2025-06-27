172ecc8f5bcfa54c7e52144f5a6c3ffa783234f5
grian32 <grianguy32@gmail.com>
2025-06-28 00:34:19 +0300

    feat(map-editor): RENDER BUTTONS WINDOW

--- map-editor/main.go
+++ map-editor/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"github.com/AllenDang/cimgui-go/imgui"
 	g "github.com/AllenDang/giu"
 	"image/color"
 )
 
 func loop() {
 	//imgui.ShowDemoWindow()
-	g.Window("Editor").Flags(g.WindowFlagsNoCollapse | g.WindowFlagsNoMove | g.WindowFlagsNoResize | g.WindowFlagsAlwaysAutoResize).Layout(
+	var editorWindowPos imgui.Vec2
+	var editorWindowSize imgui.Vec2
+	g.Window("Editor").Flags(g.WindowFlagsNoCollapse|g.WindowFlagsNoMove|g.WindowFlagsNoResize|g.WindowFlagsAlwaysAutoResize).Layout(
+		g.Custom(func() {
+			editorWindowPos = imgui.WindowPos()
+			editorWindowSize = imgui.WindowSize()
+		}),
 		g.Style().SetStyle(g.StyleVarItemSpacing, 0, 0).SetStyle(g.StyleVarItemInnerSpacing, 0, 0).SetStyleFloat(g.StyleVarFrameBorderSize, 0).SetStyle(g.StyleVarFramePadding, 1, 1).To(
 			BuildGrid()...,
 		),
+	)
+
+	g.Window("Buttons").Pos(editorWindowPos.X, editorWindowPos.Y+editorWindowSize.Y+10).Flags(g.WindowFlagsNoCollapse | g.WindowFlagsNoMove | g.WindowFlagsNoResize | g.WindowFlagsAlwaysAutoResize).Layout(
+		g.Button("Load Textures").OnClick(func() {
+			fmt.Println("loading tex")
+		}),
 	)
 }
 
