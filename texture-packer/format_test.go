f9d2682d6b73ca04660836717cd11f7dace08ff2
grian32 <grianguy32@gmail.com>
2025-06-28 14:59:37 +0300

    test(texture-packer): grpg tex header tests

---
+++ texture-packer/format_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+var buf = bytes.Buffer{}
+
+func TestWriteGRPGTexHeaderVer1(t *testing.T) {
+	expectedBytes := []byte{
+		'G', 'R', 'P', 'G', 'T', 'E', 'X', 0, // magic
+		0x00, 0x01, // ver1
+	}
+
+	err := WriteGRPGTexHeader(&buf, 1)
+	if !bytes.Equal(expectedBytes, buf.Bytes()) || err != nil {
+		t.Errorf("WriteGRPGTexHeader(&buf, 1)= %q, %v, want match for %#q", output, err, expectedBytes)
+	}
+}
+
+func TestWriteGRPGTexHeaderVerMax(t *testing.T) {
+	expectedBytes := []byte{
+		'G', 'R', 'P', 'G', 'T', 'E', 'X', 0, // magic
+		0xFF, 0xFF, // ver1
+	}
+
+	err := WriteGRPGTexHeader(&buf, 65535)
+	if !bytes.Equal(expectedBytes, buf.Bytes()) || err != nil {
+		t.Errorf("WriteGRPGTexHeader(&buf, 1)= %q, %v, want match for %#q", output, err, expectedBytes)
+	}
+}
