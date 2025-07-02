0d2e03d31192cf43849878600423e2687f5b1b2b
grian32 <grianguy32@gmail.com>
2025-07-02 21:17:14 +0300

    refactor(map-editor): use dialog library over tinyfiledialogs as it works better and has more features.

--- map-editor/textures.go
+++ map-editor/textures.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	g "github.com/AllenDang/giu"
-	"github.com/wizzymore/tinyfiledialogs"
+	"github.com/sqweek/dialog"
 	"grpg/data-go/gbuf"
 	"grpg/data-go/grpgtex"
 	"image"
@@ -31,48 +31,50 @@ func LoadTextures() {
 		log.Fatal(err)
 	}
 
-	out, got := tinyfiledialogs.OpenFileDialog("Please select a textures.pak file.", workingDir, []string{"*"}, "any", false)
-	if got && out != "" {
-		file, err := os.Open(out)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fileBytes, err := io.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		buf := gbuf.NewGBuf(fileBytes)
-		header := grpgtex.ReadHeader(buf)
-		correctMagic := "GRPGTEX\x00"
-
-		// move this to some notification system or something
-		if string(header.Magic[:]) != correctMagic {
-			log.Fatal("File entered for texture loading has the wrong magic header.")
-		} else {
-			fmt.Printf("Successfully loaded GRPGTex file with version %d\n", header.Version)
-		}
-
-		grpgTextures := grpgtex.ReadTextures(buf)
-		for _, tex := range grpgTextures {
-			pngImage, err := png.Decode(bytes.NewReader(tex.PNGBytes))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			internalId := string(tex.InternalIdString[:])
-
-			g.NewTextureFromRgba(pngImage.(*image.NRGBA), func(texture *g.Texture) {
-				textures[internalId] = GiuTextureTyped{
-					Texture:           texture,
-					InternalId:        tex.InternalIdInt,
-					FormattedStringId: fmt.Sprintf("%s(id: %d)", tex.InternalIdString, tex.InternalIdInt),
-					TextureType:       tex.Type,
-				}
-			})
-		}
-
-		BuildSelectorTypeMaps()
+	out, err := dialog.File().Title("Please select a textures.pak file").SetStartDir(workingDir).Load()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	file, err := os.Open(out)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := gbuf.NewGBuf(fileBytes)
+	header := grpgtex.ReadHeader(buf)
+	correctMagic := "GRPGTEX\x00"
+
+	// move this to some notification system or something
+	if string(header.Magic[:]) != correctMagic {
+		log.Fatal("File entered for texture loading has the wrong magic header.")
+	} else {
+		fmt.Printf("Successfully loaded GRPGTex file with version %d\n", header.Version)
+	}
+
+	grpgTextures := grpgtex.ReadTextures(buf)
+	for _, tex := range grpgTextures {
+		pngImage, err := png.Decode(bytes.NewReader(tex.PNGBytes))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		internalId := string(tex.InternalIdString[:])
+
+		g.NewTextureFromRgba(pngImage.(*image.NRGBA), func(texture *g.Texture) {
+			textures[internalId] = GiuTextureTyped{
+				Texture:           texture,
+				InternalId:        tex.InternalIdInt,
+				FormattedStringId: fmt.Sprintf("%s(id: %d)", tex.InternalIdString, tex.InternalIdInt),
+				TextureType:       tex.Type,
+			}
+		})
+	}
+
+	BuildSelectorTypeMaps()
 }
