64c466fe9319231544a39415952a83925816c75a
grian32 <grianguy32@gmail.com>
2025-07-13 06:10:29 +0300

    feat(grpgscript): initial ast gen stuff

--- grpgscript/ast/expr.go
+++ grpgscript/ast/expr.go
@@ -1,1 +1,24 @@
 package ast
+
+import "grpgscript/lex"
+
+type Expr interface {}
+
+type Binary struct {
+    Left Expr
+    Operator lex.Token
+    Right Expr
+}
+
+type Grouping struct {
+    Expression Expr
+}
+
+type Literal struct {
+    Value any
+}
+
+type Unary struct {
+    Operator lex.Token
+    Right Expr
+}
