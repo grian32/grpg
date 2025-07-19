6ceab3ee330166965c2a3d257aae0efa0c69cec4
grian32 <grianguy32@gmail.com>
2025-07-19 19:50:35 +0300

    feat(grpgscript): Evaluate integer literals

---
+++ grpgscript/evaluator/evaluator.go
@@ -0,0 +1,30 @@
+package evaluator
+
+import (
+	"fmt"
+	"grpgscript/ast"
+	"grpgscript/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	default:
+		panic(fmt.Sprintf("unexpected ast.Node: %#v", node))
+	}
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
