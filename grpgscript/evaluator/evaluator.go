bd5c107815f68fb2a50e4cd2d5194410d4e4f369
grian32 <grianguy32@gmail.com>
2025-07-19 20:01:52 +0300

    feat(grpgscript): eval bools

--- grpgscript/evaluator/evaluator.go
+++ grpgscript/evaluator/evaluator.go
@@ -6,6 +6,13 @@ import (
 	"grpgscript/object"
 )
 
+var NULL = &object.Null{}
+
+var boolLookup = map[bool]*object.Boolean{
+	true:  &object.Boolean{Value: true},
+	false: &object.Boolean{Value: false},
+}
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -14,6 +21,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return boolLookup[node.Value]
 	default:
 		panic(fmt.Sprintf("unexpected ast.Node: %#v", node))
 	}
