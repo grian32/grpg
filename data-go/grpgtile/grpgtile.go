d3894ed33ce40f94518f71db40d6ee11594e4100
grian32 <grianguy32@gmail.com>
2025-08-07 16:51:35 +0300

    feat(data-go): grpgtile format

---
+++ data-go/grpgtile/grpgtile.go
@@ -0,0 +1,68 @@
+package grpgtile
+
+import (
+	"cmp"
+	"grpg/data-go/gbuf"
+)
+
+type Header struct {
+	Magic string
+}
+
+type Tile struct {
+	Name   string
+	TileId uint16
+	TexId  uint16
+}
+
+func WriteHeader(buf *gbuf.GBuf) {
+	buf.WriteBytes([]byte("GRPGTILE"))
+}
+
+func ReadHeader(buf *gbuf.GBuf) (Header, error) {
+	bytes, err := buf.ReadBytes(8)
+	if err != nil {
+		return Header{}, err
+	}
+
+	return Header{
+		Magic: string(bytes),
+	}, nil
+}
+
+func WriteTiles(buf *gbuf.GBuf, tiles []Tile) {
+	buf.WriteUint16(uint16(len(tiles)))
+
+	for _, tile := range tiles {
+		buf.WriteString(tile.Name)
+		buf.WriteUint16(tile.TileId)
+		buf.WriteUint16(tile.TexId)
+	}
+}
+
+func ReadTiles(buf *gbuf.GBuf) ([]Tile, error) {
+	len, err := buf.ReadUint16()
+	if err != nil {
+		return nil, err
+	}
+
+	arr := make([]Tile, 0, len)
+
+	for _ = range len {
+		name, err1 := buf.ReadString()
+		tileId, err2 := buf.ReadUint16()
+		texId, err3 := buf.ReadUint16()
+
+		if err := cmp.Or(err1, err2, err3); err != nil {
+			return nil, err
+		}
+
+		arr = append(arr, Tile{
+			Name:   name,
+			TileId: tileId,
+			TexId:  texId,
+		})
+	}
+
+	return arr, nil
+}
---
+++ data-go/grpgtile/grpgtile_test.go
@@ -0,0 +1,75 @@
+package grpgtile
+
+import (
+	"bytes"
+	"grpg/data-go/gbuf"
+	"slices"
+	"testing"
+)
+
+func TestReadWriteHeader(t *testing.T) {
+	expectedHeader := Header{
+		Magic: "GRPGTILE",
+	}
+
+	expectedBytes := []byte("GRPGTILE")
+
+	buf := gbuf.NewEmptyGBuf()
+
+	t.Run("WriteHeader", func(t *testing.T) {
+		WriteHeader(buf)
+
+		if !bytes.Equal(buf.Bytes(), expectedBytes) {
+			t.Errorf("WriteHeader=%v, want=%v", buf.Bytes(), expectedBytes)
+		}
+	})
+
+	t.Run("ReadHeader", func(t *testing.T) {
+		header, err := ReadHeader(buf)
+
+		if header != expectedHeader || err != nil {
+			t.Errorf("ReadHeader=%v,%s, want=%v", header, err.Error(), expectedHeader)
+		}
+	})
+}
+
+func TestReadWriteTiles(t *testing.T) {
+	expectedTiles := []Tile{
+		{
+			Name:   "grass",
+			TileId: 1,
+			TexId:  2,
+		},
+		{
+			Name:   "water",
+			TileId: 2,
+			TexId:  6,
+		},
+	}
+
+	expectedBytes := []byte{
+		0x00, 0x02, // tile arr len
+		0x00, 0x00, 0x00, 0x05, 'g', 'r', 'a', 's', 's', // grass str
+		0x00, 0x01, 0x00, 0x02, // tileid, texid
+		0x00, 0x00, 0x00, 0x05, 'w', 'a', 't', 'e', 'r',
+		0x00, 0x02, 0x00, 0x06, // tileid, texid
+	}
+
+	buf := gbuf.NewEmptyGBuf()
+
+	t.Run("WriteTiles", func(t *testing.T) {
+		WriteTiles(buf, expectedTiles)
+
+		if !bytes.Equal(buf.Bytes(), expectedBytes) {
+			t.Errorf("WriteTiles=%v, want=%v", buf.Bytes(), expectedBytes)
+		}
+	})
+
+	t.Run("ReadTiles", func(t *testing.T) {
+		tiles, err := ReadTiles(buf)
+
+		if !slices.Equal(tiles, expectedTiles) || err != nil {
+			t.Errorf("ReadHeader=%v,%s, want=%v", tiles, err.Error(), expectedTiles)
+		}
+	})
+}
