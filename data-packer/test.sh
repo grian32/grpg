f897ed187582f4c53ab836dc0416d6688fc7422a
grian32 <grianguy32@gmail.com>
2025-08-09 01:15:53 +0300

    feat(data-packer): delete binary after creating in test.sh

--- data-packer/test.sh
+++ data-packer/test.sh
@@ -2,3 +2,4 @@
 go build -o grpgpack
 ./grpgpack tex -m testdata/test_tex_manifest.toml -o testdata/textures.grpgtex
 ./grpgpack tile -m testdata/test_tile_manifest.toml -o testdata/tiles.grpgtile -t testdata/textures.grpgtex
+rm -f grpgpack
