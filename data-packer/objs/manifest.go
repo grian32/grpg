23d44c626d2f58b3dbc3487d66e4cfb92d07e16a
grian32 <grianguy32@gmail.com>
2025-08-09 03:55:43 +0300

    feat(data-packer): obj manifest

---
+++ data-packer/objs/manifest.go
@@ -0,0 +1,84 @@
+package objs
+
+import (
+	"io"
+	"os"
+	"slices"
+
+	"grpg/data-go/grpgobj"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type ManifestConfig struct {
+	Objs []GRPGObjManifestEntry `toml:"obj"`
+}
+
+type GRPGObjManifestEntry struct {
+	Name     string   `toml:"name"`
+	ObjId    uint16   `toml:"id"`
+	Flags    []string `toml:"flags"`
+	Textures []string `toml:"textures"`
+}
+
+// Equal only meant to be used for testing, you probably shouldn't be == this type
+func (om *GRPGObjManifestEntry) Equal(other GRPGObjManifestEntry) bool {
+	return om.Name == other.Name && om.ObjId == other.ObjId && slices.Equal(om.Flags, other.Flags) && slices.Equal(om.Textures, other.Textures)
+}
+
+func BuildGRPGObjFromManifest(entries []GRPGObjManifestEntry, texMap map[string]uint16) []grpgobj.Obj {
+	objArr := make([]grpgobj.Obj, len(entries))
+
+	for idx, entry := range entries {
+		texArr := make([]uint16, len(entry.Textures))
+
+		for texIdx, tex := range entry.Textures {
+			texArr[texIdx] = texMap[tex]
+		}
+
+		objArr[idx] = grpgobj.Obj{
+			Name:     entry.Name,
+			ObjId:    entry.ObjId,
+			Flags:    flagsFromStringSlice(entry.Flags),
+			Textures: texArr,
+		}
+	}
+
+	return objArr
+}
+
+func flagsFromStringSlice(flags []string) grpgobj.ObjFlags {
+	var data grpgobj.ObjFlags = 0
+
+	for _, flag := range flags {
+		switch flag {
+		case "STATE":
+			data |= grpgobj.ObjFlags(grpgobj.STATE)
+		case "INTERACT":
+			data |= grpgobj.ObjFlags(grpgobj.INTERACT)
+		}
+	}
+
+	return data
+}
+
+func ParseManifestFile(path string) ([]GRPGObjManifestEntry, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg ManifestConfig
+	err = toml.Unmarshal(bytes, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg.Objs, nil
+}
---
+++ data-packer/objs/manifest_test.go
@@ -0,0 +1,59 @@
+package objs
+
+import (
+	"grpg/data-go/grpgobj"
+	"testing"
+)
+
+var manifest = []GRPGObjManifestEntry{
+	{
+		Name:     "water_s",
+		ObjId:    1,
+		Flags:    []string{},
+		Textures: []string{"still_water"},
+	},
+	{
+		Name:     "i_water",
+		ObjId:    2,
+		Flags:    []string{"STATE", "INTERACT"},
+		Textures: []string{"still_water", "grass_tex"},
+	},
+}
+
+var texMap = map[string]uint16{
+	"grass_tex":   2,
+	"still_water": 6,
+}
+
+func TestParseManifestFile(t *testing.T) {
+	filepath := "../testdata/test_obj_manifest.toml"
+
+	output, err := ParseManifestFile(filepath)
+
+	if len(output) != 2 || !manifest[0].Equal(output[0]) || !manifest[1].Equal(output[1]) || err != nil {
+		t.Errorf("ParseManifestFile=%v, %v, want match for %v", output, err.Error(), manifest)
+	}
+}
+
+func TestBuildGRPGObjFromManifest(t *testing.T) {
+	expectedObjs := []grpgobj.Obj{
+		{
+			Name:     "water_s",
+			ObjId:    1,
+			Flags:    0,
+			Textures: []uint16{6},
+		},
+		{
+			Name:     "i_water",
+			ObjId:    2,
+			Flags:    grpgobj.ObjFlags(grpgobj.STATE | grpgobj.INTERACT),
+			Textures: []uint16{6, 2},
+		},
+	}
+
+	output := BuildGRPGObjFromManifest(manifest, texMap)
+
+	if len(output) != 2 || !output[0].Equal(expectedObjs[0]) || !output[1].Equal(expectedObjs[1]) {
+		t.Errorf("BuildGRPGObjFromManifest=%v, want match for %v", output, expectedObjs)
+	}
+}
