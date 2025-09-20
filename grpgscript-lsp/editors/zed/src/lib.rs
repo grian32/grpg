090f00001e62459e946bfc01db38402ff646c28b
grian32 <grianguy32@gmail.com>
2025-09-20 15:12:52 +0300

    feat(grpgscript-lsp/zed): fetch grpgscriptlsp from api

--- grpgscript-lsp/editors/zed/src/lib.rs
+++ grpgscript-lsp/editors/zed/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 use zed_extension_api as zed;
 use zed_extension_api::{Command, LanguageServerId, Worktree};
 
@@ -8,11 +11,18 @@ impl zed::Extension for GRPGScriptLsp{
     where
         Self: Sized
     {
+        let lsp_path = Path::new("./grpgscriptlsp");
+        if !lsp_path.exists() {
+            let body = reqwest::blocking::get("http://51.83.129.212:4022/assets/grpgscriptlsp").unwrap().bytes().unwrap();
+            let mut file = File::create("./grpgscriptlsp").unwrap();
+            file.write_all(body.iter().as_slice()).unwrap();
+        }
+
         Self{}
     }
     fn language_server_command(&mut self, _language_server_id: &LanguageServerId, worktree: &Worktree) -> zed_extension_api::Result<Command> {
         Ok(Command {
-            command: String::from("/home/grian/IdeaProjects/grpg/grpgscript-lsp/editors/zed/grpgscriptlsp"),
+            command: String::from("./grpgscriptlsp"),
             args: vec![],
             env: worktree.shell_env(),
         })
